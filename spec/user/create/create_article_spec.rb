4586886be433191ab9e0cf0629e82ccf58cf2d34
natasha <natasha08@me.com>
2016-08-17 15:02:08 -0600

    adds create_article adds new view for articles

---
+++ spec/user/create/create_article_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature "user creates an article" do
+  let!(:user) { create :user }
+
+  before do
+    user_sign_in
+  end
+  scenario "the article is created" do
+    click_on "New article"
+    fill_in "Title", with: 'A Tale of Two Cities'
+    fill_in "Text", with: 'It was the best of times, it was the worst of times...'
+    expect do
+      click_on "Create Article"
+    end.to change { Article.count }.by 1
+  end
+end
