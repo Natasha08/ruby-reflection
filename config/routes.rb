aeec010509979ad8cb057ec5f22d658184417e61
natasha <natasha08@me.com>
2016-08-19 00:11:09 -0600

    adds comments controller with create action and private method adds comments as a dependency of articles renders comments form in articles#show makes comments a nested resource for articles adds comments factory

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'welcome#index'
 end
-
-# Rails.application.routes.draw do
-#   devise_for :users, controllers: { sessions: 'users/sessions' }
-#   resources :articles do
-#     resources :comments
-#   end
-
-# end
