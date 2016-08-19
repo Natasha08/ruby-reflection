5ae22e4260d00399ac15da24deb3e7d24123cef8
natasha <natasha08@me.com>
2016-08-18 22:55:16 -0600

    creates an create article spec adds relevant views, model and controller adds update article spec

---
+++ spec/factories/articles.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :article do
+    title "Javascript Ninja Stars"
+    text "Aim and Precision"
+  end
+end
---
+++ spec/user/update/update_article_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature "user updates an article" do
+  let!(:user) { create :user }
+  let!(:article) { create :article }
+
+  before do
+    user_sign_in
+  end
+
+  scenario "the article is updated" do
+    within "tr", text:article.title do
+    click_on "Edit"
+    end
+    click_on "Edit"
+    fill_in "Title", with: 'A Man for All Seasons'
+    fill_in "Text", with: 'Every man has his price...'
+
+    click_on "Update Article"
+    expect(page).to have_content 'Every man has his price...'
+  end
+end
