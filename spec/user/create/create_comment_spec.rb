b8f9421805f385980c596b983ab9ab89f0f05681
natasha <natasha08@me.com>
2016-08-18 23:29:07 -0600

    adds create comment_spec from ruby-mirror

---
+++ spec/user/create/create_comment_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature "user creates a comment" do
+  let!(:user) { create :user }
+  let!(:article) { create :article }
+
+  before do
+    user_sign_in
+  end
+  scenario "the comment is created" do
+    visit articles_path
+    click_on "Edit"
+    fill_in "Commenter", with: 'Newbie'
+    fill_in "Body", with: 'A classic, and one of my favorites!'
+    expect do
+      click_on "Create Comment"
+    end.to change { Comment.count }.by 1
+  end
+end
