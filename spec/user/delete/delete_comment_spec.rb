fa867bb5be925360c49448ee748ca828b1f5ba7d
natasha <natasha08@me.com>
2016-08-19 00:12:46 -0600

    adds delete_comment_spec.rb

---
+++ spec/user/delete/delete_comment_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature "user deletes a comment" do
+  let!(:user) { create :user }
+  let!(:article) { create :article }
+  let!(:comment) { create :comment }
+
+  before do
+    user_sign_in
+  end
+  scenario "the comment is deleted" do
+    visit articles_path
+    within "tr", text:article.title do
+      click_on "Edit"
+    end
+    click_on "Delete Comment"
+    expect(Comment.find_by_id(article.id)).to be_nil
+  end
+end
