b097e21a3f007471f592b49551e79d2f07f61b8e
natasha <natasha08@me.com>
2016-08-18 23:18:04 -0600

    adds delete_article_spec

---
+++ spec/user/delete/delete_article_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature "user deletes an article" do
+  let!(:user) { create :user }
+  let!(:article) { create :article }
+
+  before do
+    user_sign_in
+  end
+  scenario "the article is deleted" do
+    visit articles_path
+    within "tr", text:article.title do
+      click_on "Delete"
+    end
+    expect(Article.find_by_id(article.id)).to be_nil
+  end
+end
